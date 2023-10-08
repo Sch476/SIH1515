fcb79edce8086e4346dbcbbd33b337bc5ceff567
LostZoro476 <zoro80316@gmail.com>
2023-10-08 21:04:41 +0530

    Added Profile

--- client/Screens/Profile.js
+++ client/Screens/Profile.js
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, Image, Button } from "react-native";
 
-const Profile = () => {
+const ProfilePage = () => {
+  const handleLogout = () => {
+    // Implement your logout logic here
+    console.log("User logged out");
+    // You may want to navigate to a login screen or perform other logout actions
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: "https://placekitten.com/200/200" }}
+        style={styles.profileImage}
+      />
+      <Text style={styles.username}>John Doe</Text>
+      <Text style={styles.bio}>Software Developer</Text>
+
+      {/* Logout Button */}
+      <Button title="Logout" onPress={handleLogout} />
+
+      {/* Add more information and components as needed */}
     </View>
   );
 };
 
-export default Profile;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+  username: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  bio: {
+    fontSize: 16,
+    color: "gray",
+    marginBottom: 10,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default ProfilePage;
